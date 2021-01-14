04470fc23563edc5b30a81ec5e5749645ff5073a
jcherven <jamiecherven@gmail.com>
2021-01-14 16:13:58 -0500

    finished implementation of basic timer

--- index.js
+++ index.js
@@ -5,20 +5,38 @@ class Timer {
     this.pauseButton = pauseButton;
 
     this.startButton.addEventListener("click", this.start);
-		this.pauseButton.addEventListener('click', this.pause);
+    this.pauseButton.addEventListener("click", this.pause);
   }
 
+  // native javascript setters and getters
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value);
+  }
+
+  set timeRemaining(time) {
+    this.durationInput.value = time;
+  }
+
+  // Timer Methods
   start = () => {
     this.tick();
     this.interval = setInterval(this.tick, 1000);
   };
 
-	pause = () => {
-		clearInterval(this.interval);
-	}
+  pause = () => {
+    clearInterval(this.interval);
+  };
 
   tick = () => {
-    console.log("tick");
+    if (this.timeRemaining <= 0) {
+      this.pause();
+    } else {
+      const timeRemaining = this.timeRemaining;
+      this.timeRemaining = timeRemaining - 1;
+      // with the native js getter and setter, these two lines can be
+      // condensed like so:
+      // this.timeRemaining = this.timeRemaining - 1;
+    }
   };
 }
 
