3f51b67668e5a3722ab6258a0cc420fc3b7f98ed
jcherven <jamiecherven@gmail.com>
2021-01-14 00:04:12 -0500

    implemented a simple timer that outputs to the console

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("yubi yubi");
+class Timer {
+  constructor(durationInput, startButton, pauseButton) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+
+    this.startButton.addEventListener("click", this.start);
+		this.pauseButton.addEventListener('click', this.pause);
+  }
+
+  start = () => {
+    this.tick();
+    this.interval = setInterval(this.tick, 1000);
+  };
+
+	pause = () => {
+		clearInterval(this.interval);
+	}
+
+  tick = () => {
+    console.log("tick");
+  };
+}
+
+const durationInput = document.querySelector("#duration");
+const startButton = document.querySelector("#start");
+const pauseButton = document.querySelector("#pause");
+
+// instantiation of a Timer class
+const timer = new Timer(durationInput, startButton, pauseButton);
