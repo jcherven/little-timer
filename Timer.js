90e248553ca4da986db6a76a19a45dc7c00583bd
jcherven <jamiecherven@gmail.com>
2021-01-14 19:08:21 -0500

    separated Timer class from main script; implemented events

---
+++ Timer.js
@@ -0,0 +1,56 @@
+class Timer {
+  constructor(durationInput, startButton, pauseButton, callbacks) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+    if (callbacks) {
+      this.onStart = callbacks.onStart;
+      this.onTick = callbacks.onTick;
+      this.onComplete = callbacks.onComplete;
+    }
+
+    this.startButton.addEventListener("click", this.start);
+    this.pauseButton.addEventListener("click", this.pause);
+  }
+
+  // native javascript setters and getters
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value);
+  }
+
+  set timeRemaining(time) {
+    this.durationInput.value = time;
+  }
+
+  // Timer Methods
+  start = () => {
+    if (this.onStart) {
+      this.onStart();
+    }
+    this.tick();
+    this.interval = setInterval(this.tick, 1000);
+  };
+
+  pause = () => {
+    clearInterval(this.interval);
+  };
+
+  tick = () => {
+    if (this.timeRemaining <= 0) {
+      this.pause();
+      if (this.onComplete) {
+        this.onComplete();
+      }
+    } else {
+      const timeRemaining = this.timeRemaining;
+      this.timeRemaining = timeRemaining - 1;
+      // with the native js getter and setter, these two lines can be
+      // condensed like so:
+      // this.timeRemaining = this.timeRemaining - 1;
+      if (this.onTick) {
+        this.onTick();
+      }
+    }
+  };
+}
+
--- index.js
+++ index.js
@@ -1,48 +1,16 @@
-class Timer {
-  constructor(durationInput, startButton, pauseButton) {
-    this.durationInput = durationInput;
-    this.startButton = startButton;
-    this.pauseButton = pauseButton;
-
-    this.startButton.addEventListener("click", this.start);
-    this.pauseButton.addEventListener("click", this.pause);
-  }
-
-  // native javascript setters and getters
-  get timeRemaining() {
-    return parseFloat(this.durationInput.value);
-  }
-
-  set timeRemaining(time) {
-    this.durationInput.value = time;
-  }
-
-  // Timer Methods
-  start = () => {
-    this.tick();
-    this.interval = setInterval(this.tick, 1000);
-  };
-
-  pause = () => {
-    clearInterval(this.interval);
-  };
-
-  tick = () => {
-    if (this.timeRemaining <= 0) {
-      this.pause();
-    } else {
-      const timeRemaining = this.timeRemaining;
-      this.timeRemaining = timeRemaining - 1;
-      // with the native js getter and setter, these two lines can be
-      // condensed like so:
-      // this.timeRemaining = this.timeRemaining - 1;
-    }
-  };
-}
-
 const durationInput = document.querySelector("#duration");
 const startButton = document.querySelector("#start");
 const pauseButton = document.querySelector("#pause");
 
 // instantiation of a Timer class
-const timer = new Timer(durationInput, startButton, pauseButton);
+const timer = new Timer(durationInput, startButton, pauseButton, {
+  onStart() {
+    console.log('timer started');
+  },
+  onTick() {
+    console.log('tick');
+  },
+  onComplete() {
+    console.log('timer complete');
+  },
+});
